53cbb55063aeab78b5cc7e967afd2544da9f31ea
Diana <diana.sanchez.o@uniautonoma.edu.co>
2024-11-17 18:43:10 -0500

    Resuelto el conflicto en home/apps.py

--- home/apps.py
+++ home/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
-<<<<<<< HEAD
-
-=======
->>>>>>> ff136ca41d4557963a8e55a041aba5c7619bc678
